9da73dd0d17d96ad841531274f6b286e67cd7dcf
Robin Diddams <robindiddams@gmail.com>
2024-07-06 15:25:07 -0500

    working display!!!

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,41 @@
 #include "Adafruit_MCP9601.h"
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SH110X.h>
+
+Adafruit_SH1107 display = Adafruit_SH1107(64, 128, &Wire1);
+
+// // OLED FeatherWing buttons map to different pins depending on board:
+// #if defined(ESP8266)
+//   #define BUTTON_A  0
+//   #define BUTTON_B 16
+//   #define BUTTON_C  2
+// #elif defined(ESP32) && !defined(ARDUINO_ADAFRUIT_FEATHER_ESP32S2)
+//   #define BUTTON_A 15
+//   #define BUTTON_B 32
+//   #define BUTTON_C 14
+// #elif defined(ARDUINO_STM32_FEATHER)
+//   #define BUTTON_A PA15
+//   #define BUTTON_B PC7
+//   #define BUTTON_C PC5
+// #elif defined(TEENSYDUINO)
+//   #define BUTTON_A  4
+//   #define BUTTON_B  3
+//   #define BUTTON_C  8
+// #elif defined(ARDUINO_NRF52832_FEATHER)
+//   #define BUTTON_A 31
+//   #define BUTTON_B 30
+//   #define BUTTON_C 27
+// #else // 32u4, M0, M4, nrf52840, esp32-s2 and 328p
+//   #define BUTTON_A  9
+//   #define BUTTON_B  6
+//   #define BUTTON_C  5
+// #endif
+
+
+
+
 
 #define I2C_ADDRESS (0x67)
 
@@ -9,6 +46,40 @@ void setup() {
   while (!Serial) {
     delay(10);
   }
+
+  Serial.println("128x64 OLED FeatherWing test");
+  delay(250); // wait for the OLED to power up
+  display.begin(0x3C, true); // Address 0x3C default
+
+  Serial.println("OLED begun");
+
+  // Show image buffer on the display hardware.
+  // Since the buffer is intialized with an Adafruit splashscreen
+  // internally, this will display the splashscreen.
+  display.display();
+  delay(1000);
+
+  // Clear the buffer.
+  display.clearDisplay();
+  display.display();
+
+  display.setRotation(1);
+  Serial.println("Button test");
+
+  // pinMode(BUTTON_A, INPUT_PULLUP);
+  // pinMode(BUTTON_B, INPUT_PULLUP);
+  // pinMode(BUTTON_C, INPUT_PULLUP);
+
+  // text display tests
+  display.setTextSize(1);
+  display.setTextColor(SH110X_WHITE);
+  display.setCursor(0,0);
+  display.print("Connecting to SSID\n'adafruit':");
+  display.print("connected!");
+  display.println("IP: 10.0.1.23");
+  display.println("Sending val #0");
+  display.display(); // actually display all of the above
+
   Serial.println("Adafruit MCP9601 test");
 
   /* Initialise the driver with I2C_ADDRESS and the default I2C bus. */
@@ -76,7 +147,8 @@ void loop()
   if (status & MCP960X_STATUS_ALERT4) { Serial.print("Alert 4, "); }
   Serial.println();
   
-  Serial.print("Hot Junction: "); Serial.println(mcp.readThermocouple());
+  float temp = mcp.readThermocouple();
+  Serial.print("Hot Junction: "); Serial.println(temp);
   Serial.print("Cold Junction: "); Serial.println(mcp.readAmbient());
   Serial.print("ADC: "); Serial.print(mcp.readADC() * 2); Serial.println(" uV");
 
@@ -85,4 +157,16 @@ void loop()
 
 
   delay(1000);
+
+  yield();
+
+  display.clearDisplay();
+  display.setTextSize(2);
+  display.setTextColor(SH110X_WHITE);
+  display.setCursor(0,0);
+  display.println("found the data:");
+  display.println(temp);
+  display.display(); // actually display all of the above
+
+
 }
