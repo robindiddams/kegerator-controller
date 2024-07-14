ce9cc3614057fd09b0f449be8488ff1d45740fc5
Robin Diddams <robindiddams@gmail.com>
2024-07-14 16:13:41 -0500

    add api url

--- src/main.cpp
+++ src/main.cpp
@@ -15,10 +15,34 @@
 extern const uint8_t root_ca_pem_start[] asm("_binary_src_root_ca_pem_start");
 extern const uint8_t root_ca_pem_end[] asm("_binary_src_root_ca_pem_end");
 
-char *getRootCA()
-{
-  return (char *)root_ca_pem_start;
-}
+const char *ca =
+    "-----BEGIN CERTIFICATE-----\n"
+    "MIIEkjCCA3qgAwIBAgIQCgFBQgAAAVOFc2oLheynCDANBgkqhkiG9w0BAQsFADA/\n"
+    "MSQwIgYDVQQKExtEaWdpdGFsIFNpZ25hdHVyZSBUcnVzdCBDby4xFzAVBgNVBAMT\n"
+    "DkRTVCBSb290IENBIFgzMB4XDTE2MDMxNzE2NDA0NloXDTIxMDMxNzE2NDA0Nlow\n"
+    "SjELMAkGA1UEBhMCVVMxFjAUBgNVBAoTDUxldCdzIEVuY3J5cHQxIzAhBgNVBAMT\n"
+    "GkxldCdzIEVuY3J5cHQgQXV0aG9yaXR5IFgzMIIBIjANBgkqhkiG9w0BAQEFAAOC\n"
+    "AQ8AMIIBCgKCAQEAnNMM8FrlLke3cl03g7NoYzDq1zUmGSXhvb418XCSL7e4S0EF\n"
+    "q6meNQhY7LEqxGiHC6PjdeTm86dicbp5gWAf15Gan/PQeGdxyGkOlZHP/uaZ6WA8\n"
+    "SMx+yk13EiSdRxta67nsHjcAHJyse6cF6s5K671B5TaYucv9bTyWaN8jKkKQDIZ0\n"
+    "Z8h/pZq4UmEUEz9l6YKHy9v6Dlb2honzhT+Xhq+w3Brvaw2VFn3EK6BlspkENnWA\n"
+    "a6xK8xuQSXgvopZPKiAlKQTGdMDQMc2PMTiVFrqoM7hD8bEfwzB/onkxEz0tNvjj\n"
+    "/PIzark5McWvxI0NHWQWM6r6hCm21AvA2H3DkwIDAQABo4IBfTCCAXkwEgYDVR0T\n"
+    "AQH/BAgwBgEB/wIBADAOBgNVHQ8BAf8EBAMCAYYwfwYIKwYBBQUHAQEEczBxMDIG\n"
+    "CCsGAQUFBzABhiZodHRwOi8vaXNyZy50cnVzdGlkLm9jc3AuaWRlbnRydXN0LmNv\n"
+    "bTA7BggrBgEFBQcwAoYvaHR0cDovL2FwcHMuaWRlbnRydXN0LmNvbS9yb290cy9k\n"
+    "c3Ryb290Y2F4My5wN2MwHwYDVR0jBBgwFoAUxKexpHsscfrb4UuQdf/EFWCFiRAw\n"
+    "VAYDVR0gBE0wSzAIBgZngQwBAgEwPwYLKwYBBAGC3xMBAQEwMDAuBggrBgEFBQcC\n"
+    "ARYiaHR0cDovL2Nwcy5yb290LXgxLmxldHNlbmNyeXB0Lm9yZzA8BgNVHR8ENTAz\n"
+    "MDGgL6AthitodHRwOi8vY3JsLmlkZW50cnVzdC5jb20vRFNUUk9PVENBWDNDUkwu\n"
+    "Y3JsMB0GA1UdDgQWBBSoSmpjBH3duubRObemRWXv86jsoTANBgkqhkiG9w0BAQsF\n"
+    "AAOCAQEA3TPXEfNjWDjdGBX7CVW+dla5cEilaUcne8IkCJLxWh9KEik3JHRRHGJo\n"
+    "uM2VcGfl96S8TihRzZvoroed6ti6WqEBmtzw3Wodatg+VyOeph4EYpr/1wXKtx8/\n"
+    "wApIvJSwtmVi4MFU5aMqrSDE6ea73Mj2tcMyo5jMd6jmeWUHK8so/joWUoHOUgwu\n"
+    "X4Po1QYz+3dszkDqMp4fklxBwXRsW10KXzPMTZ+sOPAveyxindmjkW8lGy+QsRlG\n"
+    "PfZ+G6Z6h7mjem0Y+iWlkYcV4PIWL1iwBi8saCbGS5jN2p8M+X+Q7UNKEkROb3N6\n"
+    "KOqkqm57TH2H3eDJAkSnh6/DNFu0Qg==\n"
+    "-----END CERTIFICATE-----\n";
 
 // Not sure if NetworkClientSecure checks the validity date of the certificate.
 // Setting clock just to be sure...
@@ -71,7 +95,7 @@ void connectWIFI()
   setClock();
 }
 
-bool postTemperature()
+bool postTemperature(float temp)
 {
   if ((wifiMulti.run() == WL_CONNECTED))
   {
@@ -79,14 +103,17 @@ bool postTemperature()
     HTTPClient http;
 
     Serial.print("[HTTP] begin...\n");
-    // configure traged server and url
-    char *ca = getRootCA();
-    http.begin("https://www.howsmyssl.com/a/check", ca); // HTTPS
-    // http.begin("http://httpbin.net/ip"); // HTTP
+    http.begin("https://www.howcoldismy.beer/api/temp", ca); // HTTPS
 
     Serial.print("[HTTP] GET...\n");
-    // start connection and send HTTP header
-    int httpCode = http.GET();
+    http.addHeader("Content-Type", "application/json");
+    http.addHeader("x-api-key", API_KEY);
+
+    String payload = "{\"temp\":\"";
+    payload += temp;
+    payload += "\"}";
+
+    int httpCode = http.POST(payload);
 
     // httpCode will be negative on error
     if (httpCode > 0)
@@ -123,15 +150,16 @@ float readTemperature()
   Serial.print("MCP Status: 0x");
   Serial.print(status, HEX);
   Serial.print(": ");
+  float temp = 0.0;
   if (status & MCP9601_STATUS_OPENCIRCUIT)
   {
     Serial.println("Thermocouple open!");
-    return 0.0; // don't continue, since there's no thermocouple
+    return temp; // don't continue, since there's no thermocouple
   }
   if (status & MCP9601_STATUS_SHORTCIRCUIT)
   {
     Serial.println("Thermocouple shorted to ground!");
-    return 0.0; // don't continue, since the sensor is not working
+    return temp; // don't continue, since the sensor is not working
   }
   if (status & MCP960X_STATUS_ALERT1)
   {
@@ -151,7 +179,7 @@ float readTemperature()
   }
   Serial.println();
 
-  float temp = mcp.readThermocouple();
+  temp = mcp.readThermocouple();
   Serial.print("Hot Junction: ");
   Serial.println(temp);
   Serial.print("Cold Junction: ");
@@ -417,6 +445,7 @@ void scanWIFI()
 }
 
 int scanned = 0;
+float prevTemp = 0.0;
 
 void loop()
 {
@@ -424,8 +453,6 @@ void loop()
   float temp = readTemperature();
 
   delay(1000);
-
-  yield();
 
   display.clearDisplay();
   display.setTextSize(2);
@@ -435,5 +462,13 @@ void loop()
   display.println(temp);
   display.display(); // actually display all of the above
 
-  postTemperature();
+  if (temp > prevTemp + 1.0 || temp < prevTemp - 1.0)
+  {
+    Serial.println("Temperature changed by more than 1 degree");
+    postTemperature(temp);
+  }
+
+  prevTemp = temp;
+
+  yield();
 }
